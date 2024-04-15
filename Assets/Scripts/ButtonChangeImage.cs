8b9073f9158eefcddc864662050418c5ea143b27
Mopsik62 <74981499+Mopsik62@users.noreply.github.com>
2024-04-15 19:20:12 +0300

    Improved mute button

---
+++ Assets/Scripts/ButtonChangeImage.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonChangeImage : MonoBehaviour
+{
+    // Решил вынести в отдельный скрипт ибо техничнее будет.
+    public Sprite firstButtonSprite;
+    public Sprite secondButtonSprite;
+    public Button button;
+    [SerializeField] private bool mute = false;
+
+
+    void Start()
+    {
+        if (button == null)
+        {
+          button = GetComponentInParent<Button>();
+        }
+    }
+    public void ChangeButtonImage()
+    {
+        if (mute)
+        {
+            button.image.sprite = firstButtonSprite;
+            mute = false;
+        } else
+        {
+            button.image.sprite = secondButtonSprite;
+            mute=true;
+        }
+        //Debug.Log("Asdsadasd");
+    }
+}
